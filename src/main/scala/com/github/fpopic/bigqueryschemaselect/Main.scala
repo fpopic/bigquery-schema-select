9b8d3ace6e1069fe26e6603ed11cbd6f41ffe238
Filip Popić <filip.popic@gmail.com>
2021-09-21 14:03:30 +0200

    Fix useSnakeCase flag

--- src/main/scala/com/github/fpopic/bigqueryschemaselect/Main.scala
+++ src/main/scala/com/github/fpopic/bigqueryschemaselect/Main.scala
@@ -6,7 +6,7 @@ import scala.util.{Failure, Success, Try}
 object Main {
 
   def main(args: Array[String]): Unit = {
-    val useSnakeCase = args(0).trim == "--use_snake_case"
+    val useSnakeCase = args.nonEmpty && args(0).trim == "--use_snake_case"
     val input = Source.fromInputStream(System.in).getLines().mkString
     val tryOutput: Try[String] = generateBigQuerySelectClause(input, useSnakeCase)
     tryOutput match {
