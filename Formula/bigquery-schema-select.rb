a1da4c5aa6f83838e3cddca2215ee2663d0d5139
fpopic <filip.popic@gmail.com>
2026-03-14 23:09:43 +0100

    Release: update Homebrew formula for v1.3

--- Formula/bigquery-schema-select.rb
+++ Formula/bigquery-schema-select.rb
@@ -1,8 +1,8 @@
 class BigquerySchemaSelect < Formula
   desc "Generates SQL query that selects all fields from a BigQuery schema"
   homepage "https://github.com/fpopic/bigquery-schema-select"
-  url "https://github.com/fpopic/bigquery-schema-select/archive/refs/tags/v1.2.tar.gz"
-  sha256 "beb6e8f7333c33e798a0fac2d37c2c0a69764ddb2689f173768e604a5f363c39"
+  url "https://github.com/fpopic/bigquery-schema-select/archive/refs/tags/v1.3.tar.gz"
+  sha256 "241da3b4f505334b64f231c98aeb61974c3100077e2db78d8a60da2856b8c4a4"
   license "Apache-2.0"
 
   depends_on "jq"
