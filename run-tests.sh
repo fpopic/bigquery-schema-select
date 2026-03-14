90dca8910f98c247ab84da181617ccee37138f60
fpopic <filip.popic@gmail.com>
2026-03-14 08:40:49 +0100

    Reorganize to packaged structure (bin/ directory)

--- run-tests.sh
+++ run-tests.sh
@@ -7,15 +7,15 @@ set -e
 echo "Running tests for bq-schema-select.sh..."
 
 echo "Test 1: my_schema.json"
-./bq-schema-select.sh < test-resources/my_schema.json | diff - test-resources/my_select.sql
+./bin/bq-schema-select < test-resources/my_schema.json | diff - test-resources/my_select.sql
 echo "Test 1 passed!"
 
 echo "Test 2: my_camel_schema.json --use_snake_case"
-./bq-schema-select.sh --use_snake_case < test-resources/my_camel_schema.json | diff - test-resources/my_camel_select.sql
+./bin/bq-schema-select --use_snake_case < test-resources/my_camel_schema.json | diff - test-resources/my_camel_select.sql
 echo "Test 2 passed!"
 
 echo "Test 3: my_camel_short_schema.json --use_snake_case"
-./bq-schema-select.sh --use_snake_case < test-resources/my_camel_short_schema.json | diff - test-resources/my_camel_short_select.sql
+./bin/bq-schema-select --use_snake_case < test-resources/my_camel_short_schema.json | diff - test-resources/my_camel_short_select.sql
 echo "Test 3 passed!"
 
 echo "All tests passed!"
